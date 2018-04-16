043f75bcce53da7c62c9b28cab651d27c8dcda5c
biny <bin_yang@zju.edu.cn>
2018-04-16 15:55:30 +0800

    add a python file

---
+++ test.py
@@ -0,0 +1,17 @@
+import threading
+import asyncio
+
+async def hello1():
+    print('Hello world!')
+    await hello2()
+    print('Hello again!')
+
+async def hello2():
+    print('Hello world!2')
+    await asyncio.sleep(2)
+    print('Hello again!2 ')
+
+loop = asyncio.get_event_loop()
+tasks = [hello1()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
